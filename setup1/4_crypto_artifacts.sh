42cef1aeefb4ba5c95f67953948dd24f19494259
Ubuntu <ubuntu@vm4.zc1qvsmruhquvmqgacsj3c3hxe.bx.internal.cloudapp.net>
2021-01-02 07:35:54 +0000

    Distribute Crypto Material

---
+++ setup1/4_crypto_artifacts.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+./4_prime_crypto_material_generate.sh 1
+sleep 3
+./4_prime_crypto_material_generate.sh 2
+sleep 3
+./4_prime_crypto_material_generate.sh 3
+sleep 3
+./4_prime_crypto_material_generate.sh 4
+sleep 3
+./4_prime_crypto_material_generate.sh artifacts
