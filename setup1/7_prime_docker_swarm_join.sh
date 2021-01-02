0b552b1a1c829d52756e166482e30e713cd7cefe
Ubuntu <ubuntu@vm4.zc1qvsmruhquvmqgacsj3c3hxe.bx.internal.cloudapp.net>
2021-01-02 06:33:49 +0000

    Distribute Crypto Material

--- setup1/7_prime_docker_swarm_join.sh
+++ setup1/7_prime_docker_swarm_join.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
- docker swarm join --token SWMTKN-1-5a7ccvnp8xemfgf94u3ntektin682388zy1jd2mfkgk3hs1hgq-3a0br50i68ora47tangyoz5ik 168.62.175.188:2377 --advertise-addr $1
+     docker swarm join --token SWMTKN-1-54g8o0we0w5hawgktaxxzffnow4f33s3n7m14qa2tnlcxvteog-9l2o80nuwjca922orffaqj6pp 168.62.175.188:2377 --advertise-addr $1
