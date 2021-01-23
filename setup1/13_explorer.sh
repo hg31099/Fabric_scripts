2ee34288485bcb7f364d76118ad50b362de14a2a
Ubuntu <ubuntu@VM1.yb2olm0scm0ezgx55rl2gl4ogh.bx.internal.cloudapp.net>
2021-01-23 17:28:41 +0000

    Distribute Crypto Material

--- setup1/13_explorer.sh
+++ setup1/13_explorer.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 pushd ../explorer-app
-./create-org-folder.sh
+# ./create-org-folder.sh
 sleep 3
-# docker-compose -f ./docker-compose.yaml down
+docker-compose -f ./docker-compose.yaml down
 # sleep 2
-# docker volume rm explorer-app_pgdata
+docker volume rm explorer-app_pgdata
+docker volume rm explorer-app_walletstore
 
-# sleep 2
-# docker-compose -f ./docker-compose.yaml up -d
+sleep 2
+docker-compose -f ./docker-compose.yaml up -d
 popd
