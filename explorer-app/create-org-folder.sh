b66e67d37a90b120c1bcc4e726b86a807dea02bb
Ubuntu <ubuntu@VM1.yb2olm0scm0ezgx55rl2gl4ogh.bx.internal.cloudapp.net>
2021-01-23 13:15:09 +0000

    Distribute Crypto Material

--- explorer-app/create-org-folder.sh
+++ explorer-app/create-org-folder.sh
@@ -3,13 +3,12 @@ mkdir farmersAssociation.example.com
 mkdir wholesalersAssociation.example.com
 mkdir retailersAssociation.example.com
 cd ../..
-cd ../setup1/vm1/crypto-config/peerOrganizations
-cp -r farmersAssociation.example.com/* ../../../../explorer-app/organizations/peerOrganizations
-cd ../../../vm2/crypto-config/peerOrganizations
-cp -r wholesalersAssociation.example.com/* ../../../../explorer-app/organizations/peerOrganizations
-cd ../../../vm3/crypto-config/peerOrganizations
-cp -r retailersAssociation.example.com/* ../../../../explorer-app/organizations/peerOrganizations
+cd ../setup1/vm1/crypto-config/peerOrganizations/
+cp -r farmersAssociation.example.com/* ../../../../explorer-app/organizations/peerOrganizations/farmersAssociation.example.com
+cd ../../../vm2/crypto-config/peerOrganizations/
+cp -r wholesalersAssociation.example.com/* ../../../../explorer-app/organizations/peerOrganizations/wholesalersAssociation.example.com
+cd ../../../vm3/crypto-config/peerOrganizations/
+cp -r retailersAssociation.example.com/* ../../../../explorer-app/organizations/peerOrganizations/retailersAssociation.example.com
 
-cd organizations/ordererOrganizations
 cd ../../../vm4/crypto-config/ordererOrganizations
-cp -r example.com/* ../../../../explorer-app/organizations/ordererOrganizations
+cp -r example.com/* ../../../../explorer-app/organizations/ordererOrganizations	 
--- setup1/13_explorer.sh
+++ setup1/13_explorer.sh
@@ -3,6 +3,10 @@
 pushd ../explorer-app
 ./create-org-folder.sh
 sleep 3
-docker volume rm explorer-app_pgdata
-docker-compose -f ./docker-compose.yaml up -d
+# docker-compose -f ./docker-compose.yaml down
+# sleep 2
+# docker volume rm explorer-app_pgdata
+
+# sleep 2
+# docker-compose -f ./docker-compose.yaml up -d
 popd
